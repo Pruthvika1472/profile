f35c3c4efabb623b7c045eb74a864e93b26b57d3
Pruthvika S <85168733+Pruthvika1472@users.noreply.github.com>
2022-10-19 18:31:38 +0530

    Delete jquery.js

--- jquery.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  $('#text-drop').mouseleave(function(){
-    $(this).removeClass('clicked');
-  }).click(function(){
-    $(this).addClass('clicked').html($(this).html());
-  });
-});
